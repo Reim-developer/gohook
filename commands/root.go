40ab8eef38b49683f46f450d18e214d524cfbcad
Reim-developer <contact.kaxtr@gmail.com>
2025-06-27 17:14:54 +0700

    Add root command implementation for CLI webhook sender

---
+++ commands/root.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"gohook/commands/webhook"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCommand = &cobra.Command{
+	Use:   "gohook",
+	Short: "Webhook sender with Golang",
+	Long:  "GoHook is a Command Line Interface for Discord webhook sender",
+}
+
+func Execute() {
+	rootCommand.AddCommand(webhook.WebhookCommand())
+
+	if err := rootCommand.Execute(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
