1e0b697755128ecc4b99056406f0d838a34f2185
Reim-developer <contact.kaxtr@gmail.com>
2025-06-30 21:03:41 +0700

    Refactor utils.go to improve GetHexColor function with enhanced error handling and code organization

--- core/utils.go
+++ core/utils.go
@@ -1,6 +1,24 @@
 package core
 
-import "os"
+import (
+	"os"
+
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func GetHexColor(colorStr string) (int, error) {
+
+	color, err := colorful.Hex(colorStr)
+	if err != nil {
+		return 0, err
+	}
+
+	r := int(color.R * 255)
+	g := int(color.G * 255)
+	b := int(color.B * 255)
+
+	return (r<<16 | g<<8 | b), nil
+}
 
 func FileExists(path string) bool {
 	_, err := os.Stat(path)
