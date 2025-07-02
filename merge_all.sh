41470ae368b296b9ea0f13d17650adaea5d1b2c3
Reim-developer <contact.kaxtr@gmail.com>
2025-07-02 23:11:21 +0700

    Add merge_all script for automated branch merging

---
+++ merge_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function check_tools() {
+    if ! command -v git /dev/null; then
+        echo "Coult not find Git in your operating system. Aborting."
+        exit 1
+    fi 
+}
+
+function merge_all() {
+    git switch master
+    git merge dev master
+    git push origin master
+
+    git switch stable
+    git merge master stable
+    git push origin stable
+
+}
+
+function main() {
+    check_tools
+    merge_all
+}
+
+main
