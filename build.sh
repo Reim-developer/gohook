504e1e9ea480c991fd047ffbee7931955575cec8
Reim-developer <contact.kaxtr@gmail.com>
2025-07-02 11:16:48 +0700

    Fix build script, add tar/zip support

--- build.sh
+++ build.sh
@@ -6,6 +6,16 @@ function tool_check() {
     if ! command -v go > /dev/null; then 
         echo "Could not find Go in your operating system. Aborting."
         exit 1
+    fi
+
+    if ! command -v zip > /dev/null; then
+        echo "Could not find zip in  your operating system. Aborting."
+        exit 1
+    fi
+
+    if ! command -v tar > /dev/null; then
+        echo "Could not find tar in your operating system. Aborting."
+        exit 1
     fi 
 }
 
@@ -13,9 +23,15 @@ function build() {
     local OS_LIST=("linux" "darwin" "windows")
     local OS_ARCH="amd64"
     local DIST="dist"
+    local ZIP_DIST="$DIST/zip"
+
 
     if [ ! -d "$DIST" ]; then 
         mkdir -p "$DIST"
+    fi 
+
+    if [ ! -d "$ZIP_DIST" ]; then
+        mkdir -p "$ZIP_DIST"
     fi 
 
     for OS in "${OS_LIST[@]}"; do 
@@ -25,8 +41,17 @@ function build() {
             OUTPUT="${OUTPUT}.exe"
         fi 
 
-        echo "[!] Building for $OS/$ARCH -> $OUTPUT"
+        echo "[!] Building for $OS/$OS_ARCH -> $OUTPUT"
+
         GOOS=$OS GOARCH=$OS_ARCH go build -o "$DIST/$OUTPUT"
+        
+        if [[ "$OS" == "windows" ]];then
+            zip -j "$ZIP_DIST/GoHook_Windows.zip" "$DIST/$OUTPUT"
+
+        else
+            tar -czvf "$ZIP_DIST/GoHook_$OS.tar.gz" "$DIST/$OUTPUT"
+        
+        fi 
     done
 
     echo "Build success."
