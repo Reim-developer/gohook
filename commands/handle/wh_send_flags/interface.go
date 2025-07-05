8173f8ba081dc46177b183d4de50c9a726f29e1e
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 13:51:19 +0700

    Add WebhookSendFlagInterface for handling various webhook operations

---
+++ commands/handle/wh_send_flags/interface.go
@@ -0,0 +1,12 @@
+package whsendflags
+
+import "gohook/core"
+
+type WebhookSendFlagInterface interface {
+	HandleDryRun(payload *core.DiscordWebhook)
+	HandleWebhookSendOnce(payload *core.DiscordWebhook)
+	HandleVerbose(payload *core.DiscordWebhook)
+	HandleExplicit(payload *core.DiscordWebhook)
+	HandleLoopSend(payload *core.DiscordWebhook)
+	HandleExportToJson(payload *core.DiscordWebhook)
+}
