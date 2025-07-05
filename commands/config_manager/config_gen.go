62514311aff36c3060799a684588e3ab7ea0d3be
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 23:54:45 +0700

    Add GenTomlConfigCommand for generating TOML configuration

---
+++ commands/config_manager/config_gen.go
@@ -0,0 +1,36 @@
+package config_manager
+
+import (
+	"gohook/commands/handle"
+
+	"github.com/spf13/cobra"
+)
+
+type CobraClosure = func(cmd *cobra.Command, args []string)
+
+func handleClosure() CobraClosure {
+	function := func(cmd *cobra.Command, args []string) {
+		var tomlConfigName = args[0]
+
+		var arguments = handle.GenCfgCommand{
+			TomlConfigName: tomlConfigName,
+		}
+		handle.HandleGenCfgCommand(&arguments)
+	}
+
+	return function
+}
+
+func GenTomlConfigCommand() *cobra.Command {
+	var function = handleClosure()
+
+	var genTomlCommand = &cobra.Command{
+		Use:   "gen-cfg <TOML_CONFIG_PATH>",
+		Short: "Generate TOML configuration for webhook send by",
+		Args:  cobra.ExactArgs(1),
+
+		Run: function,
+	}
+
+	return genTomlCommand
+}
