5ac2b3d214e130d676b17e02a32826a316a94f62
Reim-developer <contact.kaxtr@gmail.com>
2025-07-05 23:55:06 +0700

    Refactor HandleExportToJson to improve readability and maintainability

--- commands/handle/wh_send_flags/toJson.go
+++ commands/handle/wh_send_flags/toJson.go
@@ -13,28 +13,30 @@ type ToJsonContext struct {
 }
 
 func (context *ToJsonContext) HandleExportToJson(payload *core.DiscordWebhook) {
-	var buffer bytes.Buffer
-	var encoder = json.NewEncoder(&buffer)
+	if context.IsEnableMode {
+		var buffer bytes.Buffer
+		var encoder = json.NewEncoder(&buffer)
 
-	encoder.SetEscapeHTML(false)
-	encoder.SetIndent("", " ")
+		encoder.SetEscapeHTML(false)
+		encoder.SetIndent("", " ")
 
-	err := encoder.Encode(payload)
-	if err != nil {
+		err := encoder.Encode(payload)
+		if err != nil {
 
-		core.CriticalShow("Could not decode JSON: %s", err)
-		os.Exit(core.JsonDecodeError)
+			core.CriticalShow("Could not decode JSON: %s", err)
+			os.Exit(core.JsonDecodeError)
+		}
+
+		var timeNow = core.GetTimeNow()
+		var filePath = fmt.Sprintf("%s.json", timeNow)
+		var contentBytes = buffer.Bytes()
+
+		write_err := core.WriteTo(filePath, contentBytes)
+		if write_err != nil {
+			core.CriticalShow("Export to JSON FAILED with error: %s", write_err)
+			os.Exit(core.WriteJsonFailed)
+		}
+
+		core.InfoShow("Successfully export your payload to: %s", filePath)
 	}
-
-	var timeNow = core.GetTimeNow()
-	var filePath = fmt.Sprintf("%s.json", timeNow)
-	var contentBytes = buffer.Bytes()
-
-	write_err := core.WriteTo(filePath, contentBytes)
-	if write_err != nil {
-		core.CriticalShow("Export to JSON FAILED with error: %s", write_err)
-		os.Exit(core.WriteJsonFailed)
-	}
-
-	core.InfoShow("Successfully export your payload to: %s", filePath)
 }
