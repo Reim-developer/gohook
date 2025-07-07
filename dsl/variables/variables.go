430224de785164eccbbdf868b022ca829332b63d
Reim-developer <contact.kaxtr@gmail.com>
2025-07-07 15:58:39 +0700

    Refactor variable initialization order in GoHookVariables for consistency

--- dsl/variables/variables.go
+++ dsl/variables/variables.go
@@ -2,12 +2,14 @@ package variables
 
 import (
 	"gohook/core"
+	"gohook/dsl/variables/git_vars"
 	"gohook/dsl/variables/utils_vars"
 )
 
 var GoHookVariables = core.VariablesFunc{
-	"$TIME_NOW":      utils_vars.TimeNow(),
-	"$USER_HOME":     utils_vars.UserHome(),
-	"$USER_OS":       utils_vars.UserOS(),
-	"$USER_HOSTNAME": utils_vars.HostName(),
+	"$TIME_NOW":         utils_vars.TimeNow(),
+	"$USER_HOME":        utils_vars.UserHome(),
+	"$USER_OS":          utils_vars.UserOS(),
+	"$USER_HOSTNAME":    utils_vars.HostName(),
+	"$LAST_COMMIT_HASH": git_vars.GetLastCommitHash(),
 }
