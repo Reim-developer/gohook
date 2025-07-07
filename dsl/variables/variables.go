c73520c9556ebd88d35d814cf7110de6e949505b
Reim-developer <contact.kaxtr@gmail.com>
2025-07-07 18:22:56 +0700

    Refactor ParseVariables function to accept ModeContext and update last commit hash retrieval

--- dsl/variables/variables.go
+++ dsl/variables/variables.go
@@ -2,14 +2,19 @@ package variables
 
 import (
 	"gohook/core"
+	"gohook/dsl"
 	"gohook/dsl/variables/git_vars"
 	"gohook/dsl/variables/utils_vars"
 )
 
-var GoHookVariables = core.VariablesFunc{
-	"$TIME_NOW":         utils_vars.TimeNow(),
-	"$USER_HOME":        utils_vars.UserHome(),
-	"$USER_OS":          utils_vars.UserOS(),
-	"$USER_HOSTNAME":    utils_vars.HostName(),
-	"$LAST_COMMIT_HASH": git_vars.GetLastCommitHash(),
+func ParseVariables(context *dsl.ModeContext) core.VariablesFunc {
+	var GoHookVariables = core.VariablesFunc{
+		"$TIME_NOW":         utils_vars.TimeNow(),
+		"$USER_HOME":        utils_vars.UserHome(),
+		"$USER_OS":          utils_vars.UserOS(),
+		"$USER_HOSTNAME":    utils_vars.HostName(),
+		"$LAST_COMMIT_HASH": git_vars.GetLastCommitHash(context),
+	}
+
+	return GoHookVariables
 }
